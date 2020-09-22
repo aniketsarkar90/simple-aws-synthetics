cb6320b0687aa409986a672b3f7ecb5a4823a52a
Aniket Sarkar <aniketsarkar90@gmail.com>
2020-09-22 02:05:01 -0400

    updated script
    added schedule (10 min duration)

--- lib/simple-aws-synthetics-stack.ts
+++ lib/simple-aws-synthetics-stack.ts
@@ -1,8 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as synthetics from '@aws-cdk/aws-synthetics';
 import { Schedule } from '@aws-cdk/aws-events';
-import { Duration } from '@aws-cdk/core';
-import { Test, Code } from '@aws-cdk/aws-synthetics';
 
 
 export class SimpleAwsSyntheticsStack extends cdk.Stack {
@@ -10,7 +8,8 @@ export class SimpleAwsSyntheticsStack extends cdk.Stack {
     super(scope, id, props);
 
     // To supply the code from your local filesystem
-    const canary1 = new synthetics.Canary(this, 'MyCanary', {
+    const canary = new synthetics.Canary(this, 'MyCanary', {
+      schedule: Schedule.rate(cdk.Duration.minutes(10)), //duration is part of aws-cdk/core
       test: synthetics.Test.custom({
         //code: new synthetics.AssetCode(__dirname+'/nodejs/node_modules/index.js'),
         code: new synthetics.InlineCode('Handler: pageLoadBlueprint.handler, Script: "var synthetics = require("Synthetics");\nconst log = require("SyntheticsLogger");\nconst pageLoadBlueprint = async function () {\n// INSERT URL here\nconst URL = \"https://www.google.com\";\n\nlet page = await synthetics.getPage();\nconst response = await page.goto(URL, {waitUntil: "domcontentloaded", timeout: 30000});\n//Wait for page to render.\n//Increase or decrease wait time based on endpoint being monitored.\nawait page.waitFor(15000);\nawait synthetics.takeScreenshot("loaded", "loaded");\nlet pageTitle = await page.title();\nlog.info("Page title: " + pageTitle);\nif (response.status() !== 200) {\n throw \"Failed to load page!\";\n}\n};\n\nexports.handler = async () => {\nreturn await pageLoadBlueprint();\n};\n'),
@@ -20,7 +19,7 @@ export class SimpleAwsSyntheticsStack extends cdk.Stack {
     });
 
 /*
-    const canary = new synthetics.Canary(this, 'MyCanary', {
+    const canary2 = new synthetics.Canary(this, 'MyCanary', {
       test: synthetics.Test.custom({
         code: new synthetics.InlineCode(''),
         handler: 'index.handler', // must be 'index.handler'
