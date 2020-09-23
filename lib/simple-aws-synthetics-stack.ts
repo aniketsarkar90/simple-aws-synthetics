2cec0c9ed79a99f3fc1d932a01ce0ca7494baf08
Aniket Sarkar <aniketsarkar90@gmail.com>
2020-09-23 00:43:10 -0400

    create sns topic
    added the library and created a sample sns topic

--- lib/simple-aws-synthetics-stack.ts
+++ lib/simple-aws-synthetics-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as synthetics from '@aws-cdk/aws-synthetics';
 import { Schedule } from '@aws-cdk/aws-events';
+import * as sns from '@aws-cdk/aws-sns';
 
 
 export class SimpleAwsSyntheticsStack extends cdk.Stack {
@@ -12,10 +13,15 @@ export class SimpleAwsSyntheticsStack extends cdk.Stack {
       schedule: Schedule.rate(cdk.Duration.minutes(10)), //duration is part of aws-cdk/core
       test: synthetics.Test.custom({
         //code: new synthetics.AssetCode(__dirname+'/nodejs/node_modules/index.js'),
-        code: new synthetics.InlineCode('Handler: pageLoadBlueprint.handler, Script: "var synthetics = require("Synthetics");\nconst log = require("SyntheticsLogger");\nconst pageLoadBlueprint = async function () {\n// INSERT URL here\nconst URL = \"https://www.google.com\";\n\nlet page = await synthetics.getPage();\nconst response = await page.goto(URL, {waitUntil: "domcontentloaded", timeout: 30000});\n//Wait for page to render.\n//Increase or decrease wait time based on endpoint being monitored.\nawait page.waitFor(15000);\nawait synthetics.takeScreenshot("loaded", "loaded");\nlet pageTitle = await page.title();\nlog.info("Page title: " + pageTitle);\nif (response.status() !== 200) {\n throw \"Failed to load page!\";\n}\n};\n\nexports.handler = async () => {\nreturn await pageLoadBlueprint();\n};\n'),
+        code: new synthetics.InlineCode('Handler: pageLoadBlueprint.handler, Script: "var synthetics = require("Synthetics");\nconst log = require("SyntheticsLogger");\nconst pageLoadBlueprint = async function () {\n// INSERT URL here\nconst URL = \"https://www.aniketsarkar.com\";\n\nlet page = await synthetics.getPage();\nconst response = await page.goto(URL, {waitUntil: "domcontentloaded", timeout: 30000});\n//Wait for page to render.\n//Increase or decrease wait time based on endpoint being monitored.\nawait page.waitFor(15000);\nawait synthetics.takeScreenshot("loaded", "loaded");\nlet pageTitle = await page.title();\nlog.info("Page title: " + pageTitle);\nif (response.status() !== 200) {\n throw \"Failed to load page!\";\n}\n};\n\nexports.handler = async () => {\nreturn await pageLoadBlueprint();\n};\n'),
         //code: new synthetics.InlineCode(''),
         handler: 'index.handler', // must be 'index.handler'
       }),
+    });
+
+    // add new sns topic 
+    const topic = new sns.Topic(this, 'SimpleAWSSyntheticsTopic', {
+      displayName: 'Simple AWS Synthetics SNS Topic'
     });
 
 /*
